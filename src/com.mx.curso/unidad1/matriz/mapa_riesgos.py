dae0f259088d296356746eb7b7b62dd6dc1e63b6
Emilio <myemivalor@gmail.com>
2025-09-11 07:33:28 -0600

    Ejercicios Matrices

--- src/com.mx.curso/unidad1/matriz/mapa_riesgos.py
+++ src/com.mx.curso/unidad1/matriz/mapa_riesgos.py
@@ -40,18 +40,18 @@ for f in range(len(matriz)):
     for c in range(len(matriz[f])):
         if matriz[f][c] == 2:
             matriz[f][c] = 1
-        print(matriz[f][c], end="")
-    print()
 
 area_riesgo_actualizada = 0     #2
 area_precaucion_actualizada = 0 #1
 
 for f in range(len(matriz)):
     for c in range(len(matriz[f])):
+        print(matriz[f][c], end="")
         if matriz[f][c] == 2:
             area_riesgo_actualizada += 1
         elif matriz[f][c] == 1:
             area_precaucion_actualizada += 1
+    print()
 
 print(f"\nArea de Riesgo (2): {area_riesgo_actualizada}")
 print(f"Area de Precaucion (1): {area_precaucion_actualizada}")
