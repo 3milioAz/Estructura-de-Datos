58762c4d4b6be93c69543a7df46f31527f397d63
Emilio <myemivalor@gmail.com>
2025-09-25 07:22:33 -0600

    Actividad limpieza

--- src/com.mx.curso/unidad1/matriz/numpy/limpieza_datos.py
+++ src/com.mx.curso/unidad1/matriz/numpy/limpieza_datos.py
@@ -11,7 +11,17 @@ datos[2, 3] = 1000
 
 print("Datos originales:\n", datos)
 
-indices_erroneos = [0, 2]
+# Eliminar filas con valores negativos
+
+#indices_erroneos = [0, 2]
+
+indices_erroneos = []
+for f in range(datos.shape[0]):
+    for c in range(datos.shape[1]):
+        if datos[f,c] < 0:
+            indices_erroneos.append(f)
+        elif datos[f,c] > 100:
+            indices_erroneos.append(f)
 
 datos_limpios = np.delete(datos, indices_erroneos, axis=0)
 print("Datos limpios:\n", datos_limpios)
